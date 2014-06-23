3aabf356398d2bfb3aef595183f8cb47b0c41cb5
Bradford Knowlton <brad@ucanbonline.net>
2014-06-22 23:55:17 -0600

    Added FAQ Categories

--- wp-faq-plugin.php
+++ wp-faq-plugin.php
@@ -66,6 +66,7 @@ class WPFAQPlugin {
 			'publicly_queryable' => true,
 			'exclude_from_search' => false,
 			'has_archive' => true,
+			'taxonomies' => 'faq_categories',
 			'query_var' => true,
 			'can_export' => true,
 			'rewrite' => true,
@@ -73,6 +74,36 @@ class WPFAQPlugin {
 			'menu_icon' => 'dashicons-editor-help'
 		);
 		register_post_type( 'faq', $args );
+
+		$labels = array(
+			'name' => _x( 'FAQ Categories', 'faq_categories' ),
+			'singular_name' => _x( 'FAQ Category', 'faq_categories' ),
+			'search_items' => _x( 'Search FAQ Categories', 'faq_categories' ),
+			'popular_items' => _x( 'Popular FAQ Categories', 'faq_categories' ),
+			'all_items' => _x( 'All FAQ Categories', 'faq_categories' ),
+			'parent_item' => _x( 'Parent FAQ Category', 'faq_categories' ),
+			'parent_item_colon' => _x( 'Parent FAQ Category:', 'faq_categories' ),
+			'edit_item' => _x( 'Edit FAQ Category', 'faq_categories' ),
+			'update_item' => _x( 'Update FAQ Category', 'faq_categories' ),
+			'add_new_item' => _x( 'Add New FAQ Category', 'faq_categories' ),
+			'new_item_name' => _x( 'New FAQ Category', 'faq_categories' ),
+			'separate_items_with_commas' => _x( 'Separate faq categories with commas', 'faq_categories' ),
+			'add_or_remove_items' => _x( 'Add or remove faq categories', 'faq_categories' ),
+			'choose_from_most_used' => _x( 'Choose from the most used faq categories', 'faq_categories' ),
+			'menu_name' => _x( 'FAQ Categories', 'faq_categories' ),
+		);
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'show_in_nav_menus' => true,
+			'show_ui' => true,
+			'show_tagcloud' => true,
+			'show_admin_column' => false,
+			'hierarchical' => true,
+			'rewrite' => true,
+			'query_var' => true
+		);
+		register_taxonomy( 'faq_categories', array('faq'), $args );
 
 	}
 
